ac0a5e2e8ba483d4177462de27581e20857f0ab8
James Tien <southp0105@gmail.com>
2016-01-12 11:36:08 +0800

    Unified indentation in Staffbar.jsx as two spaces.

--- src/Staffbar.jsx
+++ src/Staffbar.jsx
@@ -5,37 +5,51 @@ import message from './utils/messages';
 import Gravatar from './Gravatar';
 
 const createGravatar = ( props ) => {
-	if ( null == props ) {
-		return null;
-	}
+  if ( null == props ) {
+    return null;
+  }
 
-	const { email, size, defaultImage } = props;
+  const { email, size, defaultImage } = props;
 
-	return (
-		<Gravatar
-			email={ email }
-			size={ size }
-			defaultImage={ defaultImage }
-		/>
-	);
+  return (
+    <Gravatar
+      email={ email }
+      size={ size }
+      defaultImage={ defaultImage }
+    />
+  );
+};
+
+const StaffToggle = ( props ) => {
+  const { onStaffToggle } = props;
+
+  return (
+    <a
+      href='#'
+      onClick={ onStaffToggle }
+    > Show you
+    </a>
+  );
 };
 
 const Staffbar = ( props ) => {
-	const { me, staffs } = props;
+  const { me, staffs, onStaffToggle } = props;
 
-	return (
-		<div className='rbc-staffbar'>
-			<span className='rbc-staffbar-me'>
-				{ createGravatar( me ) }
-			</span>
-			<span className='rbc-staffbar-staffs'>
-				{ _.map( staffs, createGravatar ) }
-			</span>
-			<span className='rbc-staffbar-buttons'>
-				<p> 'Show me' </p>
-			</span>
-		</div>
-	);
+  return (
+    <div className='rbc-staffbar'>
+      <span className='rbc-staffbar-me'>
+        { createGravatar( me ) }
+      </span>
+      <span className='rbc-staffbar-staffs'>
+        { _.map( staffs, createGravatar ) }
+      </span>
+      <span className='rbc-staffbar-buttons'>
+        <StaffToggle
+          onStaffToggle={ onStaffToggle }
+        />
+      </span>
+    </div>
+  );
 };
 
 export default Staffbar;
