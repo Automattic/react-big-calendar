a80d571b8352f25ae15724fa9bddc56fbb9d379a
James Tien <southp0105@gmail.com>
2016-01-22 18:14:57 +0800

    Restructure `rbc-toolbar` to fit design better.

--- src/Toolbar.jsx
+++ src/Toolbar.jsx
@@ -40,57 +40,61 @@ let Toolbar = React.createClass({
 
     return (
       <div className='rbc-toolbar'>
-        {
-          ( 0 != calendarList.length ) &&
-            <select className='rbc-toolbar-calendar-list' onChange={onCalendarChange}>
-              { calendarList.map( renderCalendarListItem ) }
-            </select>
-        }
-        <JoinButton
-          isStaff={isStaff}
-          onClickJoin={onClickJoin}
-          onClickLeave={onClickLeave}
-          joinMsg={messages.join}
-          joinedMsg={messages.joined}
-          leaveMsg={messages.leave}
-        />
-        <span className='rbc-toolbar-label'>
-          { label }
-        </span>
-
-        <span className='rbc-btn-group'>
-          <button
-            type='button'
-            onClick={this.navigate.bind(null, navigate.TODAY)}
-          >
-            {messages.today}
-          </button>
-          <button
-            type='button'
-            onClick={this.navigate.bind(null, navigate.PREVIOUS)}
-          >
-            {messages.previous}
-          </button>
-          <button
-            type='button'
-            onClick={this.navigate.bind(null, navigate.NEXT)}
-          >
-            {messages.next}
-          </button>
-        </span>
-
-        <span className='rbc-btn-group'>
+        <div className='rbc-toolbar-header'>
+          <span className='rbc-toolbar-label'>
+            { label }
+          </span>
+        </div>
+        <div className='rbc-toolbar-tools'>
           {
-            viewNames.map(name =>
-              <button type='button' key={name}
-                className={cn({'rbc-active': view === name})}
-                onClick={this.view.bind(null, name)}
-              >
-                {messages[name]}
-              </button>
-            )
+            ( 0 != calendarList.length ) &&
+              <select className='rbc-toolbar-calendar-list' onChange={onCalendarChange}>
+                { calendarList.map( renderCalendarListItem ) }
+              </select>
           }
-        </span>
+          <JoinButton
+            isStaff={isStaff}
+            onClickJoin={onClickJoin}
+            onClickLeave={onClickLeave}
+            joinMsg={messages.join}
+            joinedMsg={messages.joined}
+            leaveMsg={messages.leave}
+          />
+
+          <span className='rbc-btn-group'>
+            <button
+              type='button'
+              onClick={this.navigate.bind(null, navigate.TODAY)}
+            >
+              {messages.today}
+            </button>
+            <button
+              type='button'
+              onClick={this.navigate.bind(null, navigate.PREVIOUS)}
+            >
+              {messages.previous}
+            </button>
+            <button
+              type='button'
+              onClick={this.navigate.bind(null, navigate.NEXT)}
+            >
+              {messages.next}
+            </button>
+          </span>
+
+          <span className='rbc-btn-group'>
+            {
+              viewNames.map(name =>
+                <button type='button' key={name}
+                  className={cn({'rbc-active': view === name})}
+                  onClick={this.view.bind(null, name)}
+                >
+                  {messages[name]}
+                </button>
+              )
+            }
+          </span>
+        </div>
       </div>
     );
   },
